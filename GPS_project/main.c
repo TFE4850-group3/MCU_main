98907952b47da1a4a5297b29bea4572bcaa9cfae
Torgeir Garpestad <Torgeir Garpestad>
2016-02-24 19:40:49 +0100

    pynting på koden

--- GPS_project/main.c
+++ GPS_project/main.c
@@ -8,29 +8,64 @@
 #define USART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 #include <stdlib.h>
+#include <stdint.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-
-volatile uint8_t data_send;
+long test = 0;
 //---Message receive and message sent---
 //tx_msg contains data that you want to send
-uint8_t tx_msg[22]={0xA0, 0xA1, 0x00, 0x0F, 0x01, 0x04, 0x07, 0xDD, 0x09, 0x1B, 0x06, 0x29, 0x1D, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x0D, 0x0A};
-//rx_msg contains data that you receive
-int *rx_msg;
-//---Counters---
-//tx_count keeps track of where you are in the message	
-volatile uint8_t tx_count = 0;
-//rx_count keeps track of how big the message you receive is
-volatile uint8_t rx_count = 0;
-int data[22] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+//---------USART0 message------------
+uint8_t tx_msg0[22]={0xA0, 0xA1, 0x00, 0x0F, 0x01, 0x04, 0x07, 0xDD, 0x09, 0x1B, 0x06, 0x29, 0x1D, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x0D, 0x0A};
+//---------USART1 message------------
+uint8_t tx_msg1[22]={0xA0, 0xA1, 0x00, 0x0F, 0x01, 0x04, 0x07, 0xDD, 0x09, 0x1B, 0x06, 0x29, 0x1D, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x0D, 0x0A};
+//---------Counters USART0-----------
+volatile uint8_t tx_count0 = 0;
+volatile uint8_t rx_count0 = 0;
+//---------Counters USART1-----------
+volatile uint8_t tx_count1 = 0;
+volatile uint8_t rx_count1 = 0;
+int data0[22] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+int data1[22] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	
+//--------Decleration of functions-------
+void init();	
+	
 int main(void)
 {
 	
 	DDRB = 0xff;
-	PORTB = 0x00;
+	init();
+    while (1) 
+    {
+		asm("nop");
+		test = test +1;
+		//for (int i=0; i<22; i++) {
+		//	UDR0=tx_msg0[i];
+		//}
+		asm("nop");
+    }
+}
+
+//USART0 Rx Complete interrupt. Will only activated if RXC0 flag in UCSR0A is set
+ISR(USART0_RX_vect0){
+	PORTB |= 0x02;
+	data0[rx_count0]= UDR0;
+	rx_count0 = rx_count0 + 1;
+
+}
+
+ISR(USART1_RX_vect){
 	
+	data1[rx_count1]= UDR1;
+	rx_count1=rx_count1+1;
+}
+
+void init()
+{
+	//set GPS in reset mode (Reset is set to low)
+	PORTB = 0x00;
 	/* Set baud rate */
 	UBRR0H = (unsigned char)(BAUD_PRESCALE>>8);
 	UBRR0L = (unsigned char)BAUD_PRESCALE;
@@ -43,81 +78,14 @@ int main(void)
 	/* Set frame format: 8data, 2stop bit */
 	UCSR0C |= (1<<USBS0)|(1<<UCSZ00)|(1<<UCSZ01);
 	UCSR1C |= (1<<USBS1)|(1<<UCSZ10)|(1<<UCSZ11);
-	//Enable Global interrupt
-	
-	//Enable Recieve interrupt
-	UCSR0B |= (1<<RXCIE0);
-	//Enable interrupt if UDRE0 is empty. If UDRE0 is one, the buffer is empty, and ready to be written to.
-	//UCSR0B |= (1<<UDRIE0);
-	//Enable Recieve interrupt
-	UCSR1B |= (1<<RXCIE1);
-	//Enable interrupt if UDRE0 is empty. If UDRE0 is one, the buffer is empty, and ready to be written to.
-	//UCSR1B |= (1<<UDRIE1);
-	sei();
-
-    while (1) 
-    {
-		int a;
-		a = sizeof(data)/sizeof(data[0]);
-		for (int i=0; i<22; i++) {
-			UDR0=tx_msg[i];
-		}
-		/* Wait for empty transmit buffer */
-		//if(!(UCSR0A & (1<<UDRE0))){
-		//	PORTB = 0x01;
-		//}
-		//PORTB = 0x01;
-		//UDR0 = tx_msg[tx_count];
-		//tx_count = tx_count + 1;
-		//while ( !(UCSR1A & (1<<RXC1)) );
-		/* Get and return received data from buffer */
 		
-		//rx_msg = malloc(sizeof(data));
-		//rx_msg[rx_count]=data;
-		//rx_count=rx_count+1;
-		//PORTB ^= 0xff;
-		/* Put data into buffer, sends the data */
-		if(rx_count==22){
-			asm("nop");
-		}
-		//}
-		asm("nop");
-    }
-}
-
-//USART0 Rx Complete interrupt. Will only activated if RXC0 flag in UCSR0A is set
-ISR(USART0_RX_vect){
-	//uint8_t data = UDR0;
+	//Enable Recieve interrupt for USART0
+	UCSR0B |= (1<<RXCIE0);
+	//Enable Recieve interrupt for USART1
+	UCSR1B |= (1<<RXCIE1);
+	//Enable Global interrupt
+	sei();
+	//Activate GPS (Reset is set to high)
+	PORTB |= 0x01;
 	
-	rx_msg = malloc(sizeof(data));
-	rx_msg=data;
-	rx_count = rx_count + 1;
-
 }
-//USART0 Rx Complete interrupt. Will only activated if RXC0 flag in UCSR0A is set
-ISR(USART1_RX_vect){
-	//PORTB = 0x01;
-	data[rx_count]= UDR1;
-	//data[rx_count]= UDR1;
-	//rx_msg = malloc(sizeof(data));
-	//rx_msg[rx_count]=data;
-	rx_count=rx_count+1;
-}
-
-//USART0 Data Register Empty interrupt. Will only be activated if UDRE0 flag in UCSR0A is set.
-ISR(USART0_UDRE_vect){
-	//int n = sizeof(tx_msg)/sizeof(tx_msg[0]);
-	data_send = tx_msg[tx_count];
-	UDR0=tx_msg[tx_count];
-	if(tx_count==22){
-		tx_count = 0;
-	} else{
-		tx_count = tx_count + 1;	
-	}
-	UCSR0A &= ~(1<<UDRE0); 
-}
-
-//USART0 Tx Complete
-//ISR(USART0_TX_vect){
-//	
-//}
